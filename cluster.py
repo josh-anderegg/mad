668621e30143a5858be316316ad1a300f47d6498
josh-anderegg <josh.anderegg@gmail.com>
2025-06-18 12:55:12 +0200

    init biome clustering

---
+++ cluster.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+import rasterio
+IMAGE_PATH = 'data/images/'
+CLUSTER_PATH = 'data/clusters/'
+
+shutil.rmtree(CLUSTER_PATH)
+os.makedirs(CLUSTER_PATH, exist_ok=True)
+
+image_list = os.listdir(IMAGE_PATH)
+
+for image in os.listdir(IMAGE_PATH):
+    abs_path = IMAGE_PATH + image
+
+    with rasterio.open(abs_path) as src:
+        metadata = src.tags()
+        biome = metadata['BIOME'].lower()
+    biome = biome.replace(' ', '_')
+    biome = biome.replace('/', '_')
+    with open(CLUSTER_PATH + biome+'.txt', 'a') as file:
+        file.write(IMAGE_PATH+image+'\n')
+
