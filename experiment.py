f8fe9048bbc7345213ea54aec3cd86ededabb175
josh-anderegg <josh.anderegg@gmail.com>
2025-06-19 14:16:40 +0200

    added max number of images

--- experiment.py
+++ experiment.py
@@ -2,6 +2,8 @@ import random
 import os
 import subprocess
 random.seed(42)
+MAX_TRAIN_IMAGES = 800
+MAX_TEST_IMAGES = 50
 
 TEST_PERCENTAGE = 0.1
 for biome in os.listdir('data/clusters'):
@@ -12,10 +14,10 @@ for biome in os.listdir('data/clusters'):
     file_amount = len(files)
     random.shuffle(files)
     test_count = int(TEST_PERCENTAGE * file_amount)
-    test_files = files[:test_count]
-    model_files = files[test_count:]
+    test_files = files[:test_count][:MAX_TEST_IMAGES]
+    model_files = files[test_count:][:MAX_TRAIN_IMAGES]
     
-    result = subprocess.run(f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}" -c {file_amount} -r {42} -p 10000' , shell=True, capture_output=True, text=True)
+    result = subprocess.run(f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}" -c {len(model_files)} -r {42} -p 10000' , shell=True, capture_output=True, text=True)
     print(f"Testing for {biome_str}")
     cmd = f'./lasso_predict.py {result.stdout.replace('\n', '')} {' '.join(test_files)}'
     subprocess.run(cmd, shell=True)
