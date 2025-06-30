29c6ed67b666b67b19aae9dd121b7e705f266182
josh-anderegg <josh.anderegg@gmail.com>
2025-06-30 15:33:09 +0200

    test suite ready

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,10 +1,11 @@
 import random
 import os
 import subprocess
-MAX_TRAIN_IMAGES = 800
+MAX_TRAIN_IMAGES = 100
 MAX_TEST_IMAGES = 10
-TEST_PERCENTAGE = 0.1
+TEST_PERCENTAGE = 0.01
 PIXEL_COUNT = 5000
+
 # Requires the data to be clustered
 for biome in os.listdir('data/clusters'):
     biome_str = biome.replace('.txt', '')
@@ -24,7 +25,7 @@ for biome in os.listdir('data/clusters'):
     test_cmd_ext = f'./lasso_predict.py outputs/{biome_str}_extended/latest {' '.join(test_files)} -e'
     subprocess.run(test_cmd_ext, shell=True)
 
-    train_cmd_ext = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_super_extended" -c {len(model_files)} -p {PIXEL_COUNT} -e -v'
+    train_cmd_ext = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_super_extended" -c {len(model_files)} -p {PIXEL_COUNT} --super-extend -v'
     subprocess.run(train_cmd_ext , shell=True)
     test_cmd_ext = f'./lasso_predict.py outputs/{biome_str}_extended/latest {' '.join(test_files)} --super-extend'
     subprocess.run(test_cmd_ext, shell=True)
