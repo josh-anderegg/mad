85f2439e1a1259c16bb2acee1de0a9207c3190a2
josh-anderegg <josh.anderegg@gmail.com>
2025-06-30 10:50:01 +0200

    added pixel count parameter

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -4,7 +4,7 @@ import subprocess
 MAX_TRAIN_IMAGES = 800
 MAX_TEST_IMAGES = 10
 TEST_PERCENTAGE = 0.1
-
+PIXEL_COUNT = 5000
 # Requires the data to be clustered
 for biome in os.listdir('data/clusters'):
     biome_str = biome.replace('.txt', '')
@@ -19,17 +19,17 @@ for biome in os.listdir('data/clusters'):
     test_files = files[:test_count][:MAX_TEST_IMAGES]
     model_files = files[test_count:][:MAX_TRAIN_IMAGES]
 
-    train_cmd_ext = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_extended" -c {len(model_files)} -p 10000 -e -v'
+    train_cmd_ext = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_extended" -c {len(model_files)} -p {PIXEL_COUNT} -e -v'
     subprocess.run(train_cmd_ext , shell=True)
     test_cmd_ext = f'./lasso_predict.py outputs/{biome_str}_extended/latest {' '.join(test_files)} -e'
     subprocess.run(test_cmd_ext, shell=True)
 
-    train_cmd_ext = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_super_extended" -c {len(model_files)} -p 10000 -e -v'
+    train_cmd_ext = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_super_extended" -c {len(model_files)} -p {PIXEL_COUNT} -e -v'
     subprocess.run(train_cmd_ext , shell=True)
     test_cmd_ext = f'./lasso_predict.py outputs/{biome_str}_extended/latest {' '.join(test_files)} --super-extend'
     subprocess.run(test_cmd_ext, shell=True)
 
-    train_cmd_smp = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_simple" -c {len(model_files)} -p 10000 -v'
+    train_cmd_smp = f'./lasso_train.py {' '.join(model_files)} -o "outputs/{biome_str}_simple" -c {len(model_files)} -p {PIXEL_COUNT} -v'
     subprocess.run(train_cmd_smp , shell=True)
     test_cmd_smp = f'./lasso_predict.py outputs/{biome_str}_simple/latest {' '.join(test_files)}'
     subprocess.run(test_cmd_smp, shell=True)
