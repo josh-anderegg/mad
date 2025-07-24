963aa5f42d3c3d74066ff76fedc77774ed74c9b6
josh-anderegg <josh.anderegg@gmail.com>
2025-07-24 14:36:38 +0200

    create grid added to cli

--- package/create_grid.py
+++ package/create_grid.py
@@ -1,82 +1,41 @@
-#!/usr/bin/env python3
-from pathlib import Path
 import string
 import geopandas as gpd
 from shapely.geometry import box
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
 from tqdm import tqdm
-import argparse
 import random
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-g",
-    "--grid-size",
-    type=int,
-    default=5500,
-    help="Define the size of the grid in m (default: 5500)",
-)
-parser.add_argument(
-    "-o",
-    "--overlap-size",
-    type=float,
-    default=0.00001,
-    help="Define mininmal overlap in point percentage (default: 0.00001 = 0.1%)",
-)
-parser.add_argument(
-    "-r",
-    "--random-seed",
-    type=str,
-    default=None,
-    help="Random string used for all the randomization done.",
-)
-
-args = parser.parse_args()
-
-RANDOM_SYMBOLS = string.ascii_letters + string.digits
-if args.random_seed is None:
-    SEED = "".join(random.choices(RANDOM_SYMBOLS, k=32))
-else:
-    SEED = args.random_seed
-
-MIN_OVERLAP_RATIO = args.overlap_size
-GRID_SIZE = args.grid_size
-print(f"used seed: {SEED}")
-
-# Project it to an equidistant gridlike projection
-gdf = gpd.read_file(BASE_DIR / "data/maus/global_mining_polygons_v2.gpkg").to_crs(
-    epsg=3857
-)
-biomes = gpd.read_file(BASE_DIR / "data/Ecoregions2017/Ecoregions2017.shp").to_crs(
-    epsg=3857
-)
-sindex = gdf.sindex
-
-# Use the default borders for EPSG: 3857 https://epsg.io/3857
-minx, miny = -20037508.34, -20048966.1
-maxx, maxy = 20037508.34, 20048966.1
-
-cell_area = GRID_SIZE * GRID_SIZE
+from package import BASE_DIR
 
 
-def process_cell(coord):
+MIN_OVERLAP_RATIO = None
+NEGATIVE_INCLUSION_PROBABILITY = None
+GRID_SIZE = None
+SEED = None
+
+
+def process_cell(coord, maus_path, regions_path, NEGATIVE_INCLUSION_PROBABILITY):
+    global GRID_SIZE
+    maus_gdf = gpd.read_file(maus_path).to_crs(
+        epsg=3857
+    )
+    regions_gdf = gpd.read_file(regions_path).to_crs(
+        epsg=3857
+    )
+    sindex = maus_gdf.sindex
+    cell_area = GRID_SIZE * GRID_SIZE
     x, y = coord
     cell = box(x, y, x + GRID_SIZE, y + GRID_SIZE)
 
     # Local import of data for multiprocessing (GeoDataFrames are not picklable)
-    local_gdf = gdf
+    local_gdf = maus_gdf
     local_sindex = sindex
 
     idxs = list(local_sindex.intersection(cell.bounds))
     # Insertion of negatives
     if not idxs:
-        overlap = biomes[biomes.geometry.intersects(cell)]
-        if (
-            not overlap.empty and random.randrange(100) < 5
-        ):  # Must not be ocean and pass 5% chance
+        overlap = regions_gdf[regions_gdf.geometry.intersects(cell)]
+        if not overlap.empty and random.random() < NEGATIVE_INCLUSION_PROBABILITY:  # Must not be ocean and pass a random check
             return cell
         return None
 
@@ -89,32 +48,69 @@ def process_cell(coord):
     return None
 
 
-x_coords = np.arange(minx, maxx, GRID_SIZE)
-y_coords = np.arange(miny, maxy, GRID_SIZE)
-all_coords = [(x, y) for x in x_coords for y in y_coords]
+def parse_args(args):
+    global MIN_OVERLAP_RATIO, GRID_SIZE, SEED, NEGATIVE_INCLUSION_PROBABILITY
+    RANDOM_SYMBOLS = string.ascii_letters + string.digits
+    if args.random_seed is None:
+        SEED = "".join(random.choices(RANDOM_SYMBOLS, k=32))
+    else:
+        SEED = args.random_seed
 
-results = []
-with ProcessPoolExecutor() as executor:
-    for result in tqdm(
-        executor.map(process_cell, all_coords, chunksize=100000), total=len(all_coords)
-    ):
-        results.append(result)
+    random.seed(SEED)
+    MIN_OVERLAP_RATIO = args.overlap_size
+    GRID_SIZE = args.grid_size
+    NEGATIVE_INCLUSION_PROBABILITY = args.negative_probability
+    print(f"used seed: {SEED}")
 
-grid_cells = [cell for cell in results if cell is not None]
 
-grid_gdf = gpd.GeoDataFrame({"geometry": grid_cells}, crs="EPSG:3857").to_crs(
-    "EPSG:4326"
-)
-grid_gdf.to_file(
-    BASE_DIR / f"data/grids/grid_{GRID_SIZE}_epsg3857.gpkg",
-    layer="squares_layer",
-    driver="GPKG",
-)
-grid_gdf["min_lon"] = grid_gdf.bounds.minx
-grid_gdf["max_lon"] = grid_gdf.bounds.maxx
-grid_gdf["min_lat"] = grid_gdf.bounds.miny
-grid_gdf["max_lat"] = grid_gdf.bounds.maxy
+def full_grid():
+    global GRID_SIZE
+    # Use the default borders for EPSG: 3857 https://epsg.io/3857
+    minx, miny = -20037508.34, -20048966.1
+    maxx, maxy = 20037508.34, 20048966.1
 
-grid_gdf[["min_lon", "min_lat", "max_lon", "max_lat"]].to_csv(
-    BASE_DIR / f"data/grids/grid_{SEED}_{GRID_SIZE}_epsg3857.csv"
-)
+    x_coords = np.arange(minx, maxx, GRID_SIZE)
+    y_coords = np.arange(miny, maxy, GRID_SIZE)
+    return [(x, y) for x in x_coords for y in y_coords]
+
+
+def filter_grid(grid):
+    from itertools import repeat
+    global NEGATIVE_INCLUSION_PROBABILITY
+    maus_path = BASE_DIR / "data/maus/global_mining_polygons_v2.gpkg"
+    regions_path = BASE_DIR / "data/Ecoregions2017/Ecoregions2017.shp"
+    results = []
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        for result in tqdm(executor.map(process_cell, grid, repeat(maus_path), repeat(regions_path), repeat(NEGATIVE_INCLUSION_PROBABILITY), chunksize=100000), total=len(grid)):
+            if result is not None:
+                results.append(result)
+    return results
+
+
+def output_grid(grid):
+    global GRID_SIZE
+    grid_gdf = gpd.GeoDataFrame({"geometry": grid}, crs="EPSG:3857").to_crs(
+        "EPSG:4326"
+    )
+    # .gpkg ouput for visualization purposes
+    grid_gdf.to_file(
+        BASE_DIR / f"data/grids/grid_{SEED}_{GRID_SIZE}_epsg3857.gpkg",
+        layer="squares_layer",
+        driver="GPKG",
+    )
+    grid_gdf["min_lon"] = grid_gdf.bounds.minx
+    grid_gdf["max_lon"] = grid_gdf.bounds.maxx
+    grid_gdf["min_lat"] = grid_gdf.bounds.miny
+    grid_gdf["max_lat"] = grid_gdf.bounds.maxy
+
+    grid_gdf[["min_lon", "min_lat", "max_lon", "max_lat"]].to_csv(
+        BASE_DIR / f"data/grids/grid_{SEED}_{GRID_SIZE}_epsg3857.csv"
+    )
+
+
+def run(args):
+    parse_args(args)
+    global MIN_OVERLAP_RATIO, GRID_SIZE, SEED
+    grid = full_grid()
+    grid = filter_grid(grid)
+    output_grid(grid)
