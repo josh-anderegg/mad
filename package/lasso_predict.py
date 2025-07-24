759cec44a59e3a075cfe5e7ceffb38ca53095751
josh-anderegg <josh.anderegg@gmail.com>
2025-07-24 14:34:03 +0200

    fix lasso predict

--- package/lasso_predict.py
+++ package/lasso_predict.py
@@ -1,76 +1,79 @@
-#!/usr/bin/env python3
-
 import joblib
-import argparse
 import json
-
-from warnings import simplefilter
 import numpy as np
-
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from package.tif_utils import tif_to_vec, output_prediction, extend, super_extend
-simplefilter("ignore", category=ConvergenceWarning)
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("path", help="Path to the folder for which to perform the test.")
-parser.add_argument("images", nargs="+", help="Path to tif file or files.")
-parser.add_argument("-s", "--sigma", type=float, default=5, help="Sigma used to blur the ground truth. (default: 5)")
-parser.add_argument("-e", "--extend", action="store_true", default=False, help="Extends the values with Spectracl indices (default: False)")
-parser.add_argument("--super-extend", action="store_true", default=False, help="Extends the values with all possible Spectracl indices (default: False)")
+def predict():
+    from warnings import simplefilter
+    simplefilter("ignore", category=ConvergenceWarning)
+    global EXTEND, SUPER_EXTEND, SIGMA, PATH, IMAGES
+    lasso = joblib.load(f'{PATH}/model.pkl')
+    results = {}
+    for image in IMAGES:
+        X, y = tif_to_vec(image, 0, 0, 0, SIGMA, False)
 
-args = parser.parse_args()
-SIGMA = args.sigma
-EXTEND = args.extend
-SUPER_EXTEND = args.super_extend
+        if EXTEND:
+            X = extend(X)
+        if SUPER_EXTEND:
+            X = super_extend(X)
 
-PATH = args.path
-IMAGES = args.images
-lasso = joblib.load(f'{PATH}/model.pkl')
-results = {}
-for image in IMAGES:
-    X, y = tif_to_vec(image, 0, 0, 0, SIGMA, False)
+        y_pred = lasso.predict(X)
+        output_prediction(y_pred, image, PATH + '/test_images')
+        mse_unbalanced = mean_squared_error(y, y_pred)
+        mae_unbalanced = mean_absolute_error(y, y_pred)
+        rmse_unbalanced = np.sqrt(mse_unbalanced)
+        r2_unbalanced = r2_score(y, y_pred)
+        mse_unbalanced = mean_squared_error(y, y_pred)
 
-    if EXTEND:
-        X = extend(X)
-    if SUPER_EXTEND:
-        X = super_extend(X)
+        results['unbalanced'] = {
+            'MSE': float(mse_unbalanced),
+            'MAE': float(mae_unbalanced),
+            'RMSE': float(rmse_unbalanced),
+            'R2': float(r2_unbalanced),
+        }
 
-    y_pred = lasso.predict(X)
-    output_prediction(y_pred, image, PATH + '/test_images')
-    mse_unbalanced = mean_squared_error(y, y_pred)
-    mae_unbalanced = mean_absolute_error(y, y_pred)
-    rmse_unbalanced = np.sqrt(mse_unbalanced)
-    r2_unbalanced = r2_score(y, y_pred)
-    mse_unbalanced = mean_squared_error(y, y_pred)
+        X, y = tif_to_vec(image, 0.5, 0.1, 10000, SIGMA, True)
 
-    results['unbalanced'] = {
-        'MSE': float(mse_unbalanced),
-        'MAE': float(mae_unbalanced),
-        'RMSE': float(rmse_unbalanced),
-        'R2': float(r2_unbalanced),
-    }
+        if EXTEND:
+            X = extend(X)
+        if SUPER_EXTEND:
+            X = super_extend(X)
 
-    X, y = tif_to_vec(image, 0.5, 0.1, 10000, SIGMA, True)
+        y_pred = lasso.predict(X)
+        mse_balanced = mean_squared_error(y, y_pred)
+        mae_balanced = mean_absolute_error(y, y_pred)
+        rmse_balanced = np.sqrt(mse_balanced)
+        r2_balanced = r2_score(y, y_pred)
+        mse_balanced = mean_squared_error(y, y_pred)
+        results['balanced'] = {
+            'MSE': float(mse_balanced),
+            'MAE': float(mae_balanced),
+            'RMSE': float(rmse_balanced),
+            'R2': float(r2_balanced),
+        }
+        return results
 
-    if EXTEND:
-        X = extend(X)
-    if SUPER_EXTEND:
-        X = super_extend(X)
 
-    y_pred = lasso.predict(X)
-    mse_balanced = mean_squared_error(y, y_pred)
-    mae_balanced = mean_absolute_error(y, y_pred)
-    rmse_balanced = np.sqrt(mse_balanced)
-    r2_balanced = r2_score(y, y_pred)
-    mse_balanced = mean_squared_error(y, y_pred)
-    results['balanced'] = {
-        'MSE': float(mse_balanced),
-        'MAE': float(mae_balanced),
-        'RMSE': float(rmse_balanced),
-        'R2': float(r2_balanced),
-    }
+def output_results(results):
+    with open(f'{PATH}/train.json', 'w') as f:
+        json.dump(results, f, indent=4)
 
-with open(f'{PATH}/train.json', 'w') as f:
-    json.dump(results, f, indent=4)
+
+def parse_args(args):
+    global SIGMA, EXTEND, SUPER_EXTEND, PATH, IMAGES
+    SIGMA = args.sigma
+    EXTEND = args.extend
+    SUPER_EXTEND = args.super_extend
+
+    PATH = args.path
+    IMAGES = args.images
+
+
+def run(args):
+    parse_args(args)
+    results = predict()
+    output_results(results)
+    pass
