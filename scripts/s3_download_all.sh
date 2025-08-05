ea8f852cafc04e0bd1c8800469936fd35383d956
josh-anderegg <josh.anderegg@gmail.com>
2025-08-05 13:52:42 +0200

    download all reworked

--- scripts/s3_download_all.sh
+++ scripts/s3_download_all.sh
@@ -2,8 +2,11 @@
 
 mkdir -p "../data/s2-images"
 # Download all the metadata for the grid
-xargs -a "../data/grids/tiles.txt" -P 12 -I{} "./s3_get_metadata.sh" {} "../data/s2-images"
-cd "../data/s2-images"
-find . -maxdepth 1 -name '*.json' -exec ./s3_image_from_metadata.sh {} \;
-
+# xargs -a "tiles.txt" -P 12 -I{} "./s3_get_metadata.sh" {} "/run/media/cynik/Elements/s2-images"
+# find "/run/media/cynik/Elements/s2-images" -type f -name '*.json' | parallel -j 8 --compress --bar --group ./s3_image_from_metadata.sh {}
+find "/run/media/cynik/Elements/s2-images" -type f -name '*.json' \
+  | xargs -I {} sh -c './s3_image_from_metadata.sh "{}"'
 echo "All images downloaded!"
+final_log="../data/s2-images/combined_$(date +'%Y%m%d_%H%M%S').log"
+cat ../data/s2-images/*.tlog > "$final_log"
+rm -rf "../data/s2-images/*.tlog"
