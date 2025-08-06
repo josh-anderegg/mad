ba0d6933a94817db627d4b1af5cbd5f805d79e29
josh-anderegg <josh.anderegg@gmail.com>
2025-08-06 09:47:37 +0200

    deleted old

--- scripts/s3_gather_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INPUT_FILE="../data/grids/mgrs.txt"
-SCRIPT_TO_RUN="./s3_gather.sh"
-OUTPUT_DIR="../data/s3-images"
-
-xargs -a "$INPUT_FILE" -n 1 -I{} "$SCRIPT_TO_RUN" {} "$OUTPUT_DIR"
-echo "All tasks completed."
