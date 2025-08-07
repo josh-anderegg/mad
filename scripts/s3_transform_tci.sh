5489db302bc933132bfd059d77be5cc84e3ce51a
josh-anderegg <josh.anderegg@gmail.com>
2025-08-07 13:16:22 +0200

    s3 transform quickfix

--- scripts/s3_transform_tci.sh
+++ scripts/s3_transform_tci.sh
@@ -2,10 +2,11 @@
 
 TMP_DIR="$1"
 finished_file="$2"
-
+finished_file="${finished_file%.tif}.jp2"
 # Step 1: Convert .jp2 to .tif and resample all bands to 10m
-gdal_translate "$TMP_DIR/TCI.jp2" "$finished_file" > /dev/null
+# gdal_translate "$TMP_DIR/TCI.jp2" "$finished_file" > /dev/null
 
+mv "$TMP_DIR/TCI.jp2" "$finished_file"
 
 rm -rf "$TMP_DIR"
 
