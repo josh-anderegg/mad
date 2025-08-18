1080d747ec0631ebfc98d1522205d72938395281
josh-anderegg <josh.anderegg@gmail.com>
2025-08-18 10:49:49 +0200

    sync

--- scripts/grid_mgrs.py
+++ scripts/grid_mgrs.py
@@ -1,34 +1,18 @@
 import geopandas as gpd
-import requests
 
 # Read the Sentinel-2 tile grid (KML or KMZ)
-tiles_gdf = gpd.read_file("~/Downloads/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml")
+tiles_gdf = gpd.read_file("../data/grids/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml")
 maus = gpd.read_file("../data/maus/global_mining_polygons_v2.gpkg")  # or shapefile
 
 intersections = gpd.sjoin(tiles_gdf, maus, how="inner", predicate="intersects")
+for _, row in intersections.iterrows():
+    print(row['geometry'].bounds, row['Name'])
 tile_names = intersections['Name'].unique()
-
+geometries = [", ".join(map(str, row['geometry'].bounds)) for idx, row in intersections.iterrows()]
+geometries = set(geometries)
 found = 0
 total = len(tile_names)
-# tiles = []
-# for coords in tile_names:
-#     fst, snd, lst = int(coords[:2]), coords[2], coords[3:]
-#     found = False
-#     for i in range(1, 12):
-#         for j in range(1, 31):
-#             metadata_link = f"https://sentinel-s2-l2a.s3.amazonaws.com/tiles/{fst}/{snd}/{lst}/2019/{i}/{j}/0/metadata.xml"
-#             response = requests.head(metadata_link, allow_redirects=True, timeout=10)
-#
-#             if response.status_code == 200:
-#                 found += 1
-#                 tiles.append(coords)
-#                 found = True
-#                 break
-#         if found:
-#             break
-#     if not found:
-#         print(f"{coords} could no be found")
-#     else:
-#         print(f"{coords} found")
 with open("tiles.txt", "w") as f:
     f.write("\n".join(tile_names))
+with open("bboxes.txt", "w") as f:
+    f.write("\n".join(geometries))
