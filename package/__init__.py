05905f0969a09eec3a41311645f26bdf5f81c76d
josh-anderegg <josh.anderegg@gmail.com>
2025-09-02 09:15:25 +0200

    uniform seed generation

--- package/__init__.py
+++ package/__init__.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+import string
+import random
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / 'data'
 OUTPUT_DIR = BASE_DIR / 'outputs'
+RANDOM_SYMBOLS = string.ascii_letters + string.digits
+
+
+def random_seed():
+    return ''.join(random.choices(RANDOM_SYMBOLS, k=32))
