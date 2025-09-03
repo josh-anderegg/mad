243a4bee6a3c385114fe4fe3934f7c9afaf98754
josh-anderegg <josh.anderegg@gmail.com>
2025-09-03 16:06:21 +0200

    Band fix

--- package/s3_bands_to_image.sh
+++ package/s3_bands_to_image.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 TMP_DIR="$1"
 finished_file="$2"
+shift 2
 files=()
-
-for file in "$3"; do 
+for file in "$@"; do 
   file=$(basename $file)
   name="${file%.jp2}"
   gdal_translate "$TMP_DIR/${name}.jp2" "$TMP_DIR/${name}.tif" > /dev/null
-  files+="$TMP_DIR/${name}.tif"
+  files+=("$TMP_DIR/${name}.tif")
 done
 
-gdalbuildvrt -separate "$TMP_DIR/combined.vrt" "${files[@]}" > /dev/null 
-
-gdal_translate "$TMP_DIR/combined.vrt" "$finished_file"  > /dev/null
-
+gdalbuildvrt -separate -resolution highest "$TMP_DIR/combined.vrt" "${files[@]}" > /dev/null
+gdal_translate -ot UInt16 "$TMP_DIR/combined.vrt" "$TMP_DIR/finished.tif" > /dev/null
+gdal_translate -of JP2OpenJPEG -r cubic "$TMP_DIR/finished.tif" "$finished_file" > /dev/null
 rm -rf "$TMP_DIR"
-
